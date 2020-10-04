a0d2476c3de18689c7666478038c70aa9caa88fa
Joshua Karstendick <joshdick@gmail.com>
2020-10-04 13:49:36 -0400

    project setup

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # architects-ai
+
+An AI program to play Architects of the West Kingdom.
+
+## Installation
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+python setup.py develop
+architects-ai
+```
+
+## Running program
+
+```bash
+architects-ai
+```
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='architectsai',
+      version='0.0.1',
+      description='',
+      author='Joshua Karstendick',
+      url='http://karstendick.com',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['architectsai'],
+      install_requires=[
+          'nose',
+      ],
+      entry_points='''
+          [console_scripts]
+          architectsai=architectsai:main
+      ''',
+      packages=['architectsai'],
+)
